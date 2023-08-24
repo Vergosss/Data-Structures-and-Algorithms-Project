b8d0cd2ea55c9557165facb1629ab37e3fc18f1c
Georgios Vergos <72676680+Vergosss@users.noreply.github.com>
2023-08-25 02:03:21 +0300

    Added extra documentation.

--- 1072604_1072605_1072512_1072489_Vergos_Tsoulos_Vlachos_Gkikas/merosB_erotima_B Eyresh Elaxistou Megistou se BST.c
+++ 1072604_1072605_1072512_1072489_Vergos_Tsoulos_Vlachos_Gkikas/merosB_erotima_B Eyresh Elaxistou Megistou se BST.c
@@ -1,6 +1,8 @@
+//Libraries
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+//AVL Tree node definition
 struct Node{
 int date;
 int Volume;
@@ -8,6 +10,7 @@ int Ypsos;
 struct Node* left;
 struct Node* right;
 };
+//function declarations
 void remove_all_chars(char* str, char c);
 struct Node* create_node(int date,int Volume);
 int Ypsozigisi(struct Node* node);
@@ -19,11 +22,12 @@ struct Node* deksia_peristrofi(struct Node* node);
 int min_Volume(struct Node* riza);
 int max_Volume(struct Node* riza);
 int Count_file(FILE* fp);
+//main menu
 int main(){
 struct Node* root=(struct Node*)malloc(sizeof(struct Node));
-root=NULL;
+root=NULL;//memory allocation and initialization of AVL tree root
 FILE* f1;
-f1=fopen("agn.us.txt","r");
+f1=fopen("agn.us.txt","r");//opening file
 if(f1==NULL){
 exit(1);
 }
@@ -34,34 +38,34 @@ f1=fopen("agn.us.txt","r+");
 float Open,High,Low,Close;
 Open=High=Low=Close=0.0;
 int Volume,OpenInt;
-Volume=OpenInt=0;
+Volume=OpenInt=0;// temporary variables to hold each field of a line
 int i=0;
 int temp=0;
 char date[11];
 char z=fgetc(f1);
 while(z!=EOF)
 {
-fscanf(f1,"%s,%f,%f,%f,%f,%d,%d",date,&Open,&High,&Low,&Close,&Volume,&OpenInt);
-remove_all_chars(date,'-');
-temp=(int)atoi(date);
-root=eisagogi(root,temp,Volume);
+fscanf(f1,"%s,%f,%f,%f,%f,%d,%d",date,&Open,&High,&Low,&Close,&Volume,&OpenInt);//scan file's line
+remove_all_chars(date,'-');//convert date string to int mainting dates' order
+temp=(int)atoi(date);//cast to int
+root=eisagogi(root,temp,Volume);//till the end of the file insert for each line a node in the tree
 z=fgetc(f1);
 
 if(z=='\n'){
 i++;
 }
 }
-int choice;
-fclose(f1);
-while(1){
+int choice;//menu choice
+fclose(f1);//finish reading file
+while(1){//main menu
 printf("1 for max Volume 2 for min volume 3 for exit\n");
-scanf("%d",&choice);
+scanf("%d",&choice);//user's input choice
 switch(choice){
 	case 1:
-		printf("Max Volume is : %d\n",max_Volume(root));
+		printf("Max Volume is : %d\n",max_Volume(root));//max volume of AVL tree
 		break;
 	case 2:
-		printf("Min  Volume is : %d\n",min_Volume(root));
+		printf("Min  Volume is : %d\n",min_Volume(root));//min volume of AVL tree
 		break;
 	case 3:
 		exit(0);
@@ -70,25 +74,26 @@ switch(choice){
 return 0;
 }
 }
+//functions definitions
 int max_of_subtrees(int i1,int i2){
 if(i1>i2){
 return i1;
 }
-return i2;
+return i2;//if i1=i2 or i2>i1 then i2 is the biggest
 }
 ////
 int Ypsos(struct Node* node){
-if(node==NULL){
+if(node==NULL){//if it doesnt exist then return 0 height(1 for leaf node)
 return 0;
 }
-return node->Ypsos;
+return node->Ypsos;//find height of given tree node
 }
 ////
-int Ypsozigisi(struct Node* node){
+int Ypsozigisi(struct Node* node){//find balance of a node in the tree
 if(node==NULL){
-return 0;
+return 0;//if it doesnt exist then return 0
 }
-return (Ypsos(node->left)-Ypsos(node->right));
+return (Ypsos(node->left)-Ypsos(node->right));//node's height is the difference of the heights between its left subtree and right subtree
 
 }
 ////
@@ -99,26 +104,26 @@ create->Volume=Volume;
 create->Ypsos=1;
 create->left=NULL;
 create->right=NULL;
-return (create);
+return (create);//new tree node has given data as its first data height of 1 and no children
 
 }
 ////
 struct Node* eisagogi(struct Node* add,int date,int Volume){
 if(add==NULL){
-return create_node(date,Volume);
+return create_node(date,Volume);//if the tree is empty then create the node as its root
 }
 if(Volume<add->Volume){
-add->left=eisagogi(add->left,date,Volume);
+add->left=eisagogi(add->left,date,Volume);//if inserted node's data is smaller than current node's data then go to left subtree
 }
 else if(Volume>add->Volume){
-add->right=eisagogi(add->right,date,Volume);
+add->right=eisagogi(add->right,date,Volume);//if inserted node's data is bigger than current node's data then go to right subtree
 }
 else{
 return add;//stixeia me idia timh volume de boroun na yparxoun se dyadiko dentro anazitisis
 }
-add->Ypsos=max_of_subtrees(Ypsos(add->left),Ypsos(add->right))+1;
-int ipsozygisi=Ypsozigisi(add);
-
+add->Ypsos=max_of_subtrees(Ypsos(add->left),Ypsos(add->right))+1;//since we insert a new node to the tree the current node's height is increased by //1
+int ipsozygisi=Ypsozigisi(add);//however now the tree may be unbalanced due to the new insertion. Thus we 'weigh' the node
+//we have 4 cases to balance the tree
 if(ipsozygisi>1 && Volume<add->left->Volume){
 return deksia_peristrofi(add);
 }
@@ -138,7 +143,7 @@ return add;
 }
 ////
 struct Node* aristeri_peristrofi(struct Node* node)
-{
+{//left rotation
     struct Node* y = node->right;
     struct Node* T2 = y->left;
 
@@ -154,14 +159,16 @@ struct Node* aristeri_peristrofi(struct Node* node)
     return y;
 }
 ////
-struct Node* deksia_peristrofi(struct Node* node){
+struct Node* deksia_peristrofi(struct Node* node){//right rotation
 struct Node* x=node->left;
 struct Node* sub=x->right;
+//perform rotation
 x->right=node;
 node->left=sub;
+//update heights
 node->Ypsos=max_of_subtrees(Ypsos(node->left),Ypsos(node->right))+1;
 x->Ypsos=max_of_subtrees(Ypsos(x->left),Ypsos(x->right))+1;
-
+//return new root
 return x;
 }
 ////
@@ -188,18 +195,18 @@ return temp->date;
 }
 ////
 
-int Count_file(FILE* fp){
+int Count_file(FILE* fp){//function to count a file's lines
 char c=getc(fp);
 int count=0;
 for (c = getc(fp); c != EOF; c = getc(fp)){
-if (c == '\n'){
+if (c == '\n'){//if we encounter a new line increase count by one , i.e one more line
 count = count + 1;}
 }
 return count;
 }
 void remove_all_chars(char* str, char c) {
     char *pr = str, *pw = str;
-    while (*pr) {
+    while (*pr) {//while we have not finished scanning the string
         *pw = *pr++;
         pw += (*pw != c);
     }
