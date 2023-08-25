d10d939e83c19ba6bd3f7fa5a7dd088eb5dfc186
Georgios Vergos <72676680+Vergosss@users.noreply.github.com>
2023-08-25 15:29:32 +0300

    Added documentation.

--- 1072604_1072605_1072512_1072489_Vergos_Tsoulos_Vlachos_Gkikas/merosB_erotima_A_AVL Tree Traversal Insert Delete Searching.c
+++ 1072604_1072605_1072512_1072489_Vergos_Tsoulos_Vlachos_Gkikas/merosB_erotima_A_AVL Tree Traversal Insert Delete Searching.c
@@ -100,6 +100,7 @@ switch(choice){
 
 return 0;
 }
+//functions definitions
 struct Node* create_node(int date,int Volume){
 struct Node* create=(struct Node*)malloc(sizeof(struct Node));
 create->date=date;
@@ -107,48 +108,50 @@ create->Volume=Volume;
 create->Ypsos=1;
 create->left=NULL;
 create->right=NULL;
-return (create);
+return (create);//new tree node has given data as its first data height of 1 and no children
+
 
 }
 ////
 int Ypsozigisi(struct Node* node){
 if(node==NULL){
-return 0;
+return 0;//if it doesnt exist then return 0
 }
-return (Ypsos(node->left)-Ypsos(node->right));
+return (Ypsos(node->left)-Ypsos(node->right));//node's height is the difference of the heights between its left subtree and right subtree
 
 }
 ////
 ////
 int Ypsos(struct Node* node){
-if(node==NULL){
+if(node==NULL){//if it doesnt exist then return 0 height(1 for leaf node)
 return 0;
 }
-return node->Ypsos;
+return node->Ypsos;//find height of given tree node
 }
 ////
 int max_of_subtrees(int i1,int i2){
 if(i1>i2){
 return i1;
 }
-return i2;
+return i2;//if i1=i2 or i2>i1 then i2 is the biggest
 }
 ////
 struct Node* eisagogi(struct Node* add,int date,int Volume){
 if(add==NULL){
-return create_node(date,Volume);
+return create_node(date,Volume);//if the tree is empty then create the node as its root
 }
 if(date<add->date){
-add->left=eisagogi(add->left,date,Volume);
+add->left=eisagogi(add->left,date,Volume);//if inserted node's data is smaller than current node's data then go to left subtree
 }
 else if(date>add->date){
-add->right=eisagogi(add->right,date,Volume);
+add->right=eisagogi(add->right,date,Volume);//if inserted node's data is bigger than current node's data then go to right subtree
 }
 else{
-return add;
+return add;//stixeia me idia timh volume de boroun na yparxoun se dyadiko dentro anazitisis
 }
-add->Ypsos=max_of_subtrees(Ypsos(add->left),Ypsos(add->right))+1;
-int ipsozygisi=Ypsozigisi(add);
+add->Ypsos=max_of_subtrees(Ypsos(add->left),Ypsos(add->right))+1;//since we insert a new node to the tree the current node's height is increased by //1
+int ipsozygisi=Ypsozigisi(add);//however now the tree may be unbalanced due to the new insertion. Thus we 'weigh' the node
+//we have 4 cases to balance the tree
 if (ipsozygisi > 1 && date < add->left->date){
 return deksia_peristrofi(add);
 }
@@ -170,23 +173,30 @@ return add;
 }
 ////
 struct Node* aristeri_peristrofi(struct Node* node){
+//left rotation
 struct Node* y=node->right;
 struct Node* sub=y->left;
+// Perform rotation
 y->left=node;
 node->right=sub;
+ //  Update heights
 node->Ypsos=max_of_subtrees(Ypsos(node->left),Ypsos(node->right))+1;
 y->Ypsos=max_of_subtrees(Ypsos(y->left),Ypsos(y->right))+1;
+//return new root
 return y;
 }
 ////
 struct Node* deksia_peristrofi(struct Node* node){
+//right rotation
 struct Node* x=node->left;
 struct Node* sub=x->right;
+// Perform rotation
 x->right=node;
 node->left=sub;
+//Update heights
 node->Ypsos=max_of_subtrees(Ypsos(node->left),Ypsos(node->right))+1;
 x->Ypsos=max_of_subtrees(Ypsos(x->left),Ypsos(x->right))+1;
-
+//return new root
 return x;
 }
 ////
@@ -195,7 +205,7 @@ return x;
 
 ////
 void endodiatetagmeni_diasxisi(struct Node* riza){
-if(riza==NULL){
+if(riza==NULL){//inorder traversal in AVL Tree and BST's in general
 return;
 }
 endodiatetagmeni_diasxisi(riza->left);
@@ -231,10 +241,10 @@ while(temp->date!=date){
 
 if(temp!=NULL){
 if(date>temp->date){
-temp=temp->right; //an h hmeromhnia pou dosame einai megalyterh apo to komvo pou vriskomaste epeidh einai dyadiko dentro anazitisis pame sto deksi pedi
+temp=temp->right; //an h hmeromhnia pou dosame einai megalyterh apo to komvo pou vriskomaste epeidh einai dyadiko dentro anazitisis pame sto deksi //pedi
 }
 else{
-temp=temp->left; //an h hmeromhnia pou dosame einai mikroterh apo to komvo pou vriskomaste epeidh einai dyadiko dentro anazitisis pame sto aristero pedi
+temp=temp->left; //an h hmeromhnia pou dosame einai mikroterh apo to komvo pou vriskomaste epeidh einai dyadiko dentro anazitisis pame sto aristero //pedi
 }
 if(temp==NULL){
 return -1;
@@ -246,7 +256,7 @@ printf("Please give the new Volume:\n"); //molis ftasoume sto komvo pou theloume
 scanf("%d",&temp->Volume);
 return temp->Volume;
 }
-int Count_file(FILE* fp){
+int Count_file(FILE* fp){//count number of lines in file
 char c=getc(fp);
 int count=0;
 for (c = getc(fp); c != EOF; c = getc(fp)){
@@ -255,7 +265,7 @@ count = count + 1;}
 }
 return count;
 }
-void remove_all_chars(char* str, char c) {
+void remove_all_chars(char* str, char c) {//remove given character from string and delete the produced space
     char *pr = str, *pw = str;
     while (*pr) {
         *pw = *pr++;
@@ -373,7 +383,7 @@ struct Node* diagrafi(struct Node* add, int date)
 } 
 
 struct Node* min_node(struct Node* node) 
-{ 
+{ //node with min value in AVL tree and BStrees in general is that the leftmost leaf has the smallest value
     struct Node* temp= node; 
     while (temp->left != NULL) {
 	
